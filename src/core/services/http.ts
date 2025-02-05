3737d6302fc437e2f07717846b934d44a7e1985f
Ivan Perez Abreu <belzee92@gmail.com>
2025-02-05 12:25:52 +0100

    implement http client

---
+++ src/core/services/http.ts
@@ -0,0 +1,46 @@
+import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios';
+
+import { env } from '@/core/utils/envConfig';
+
+const headers: Readonly<Record<string, string | boolean>> = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json; charset=utf-8',
+  'Access-Control-Allow-Credentials': true,
+  'X-Requested-With': 'XMLHttpRequest',
+};
+
+class Http {
+  private instance: AxiosInstance | null = null;
+
+  private get http(): AxiosInstance {
+    return this.instance != null ? this.instance : this.initHttp();
+  }
+
+  initHttp() {
+    const http = axios.create({
+      baseURL: env.VITE_API_URL,
+      headers,
+    });
+
+    this.instance = http;
+    return http;
+  }
+
+  get<T = unknown, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.get<T, R>(url, config);
+  }
+
+  post<T = unknown, R = AxiosResponse<T>>(url: string, data?: T, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.post<T, R>(url, data, config);
+  }
+
+  put<T = unknown, R = AxiosResponse<T>>(url: string, data?: T, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.put<T, R>(url, data, config);
+  }
+
+  delete<T = unknown, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.delete<T, R>(url, config);
+  }
+}
+
+export const http = new Http();
