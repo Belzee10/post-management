243619dc989f703697fbb8d8ca05cec48123d107
Ivan Perez Abreu <belzee92@gmail.com>
2025-02-05 11:54:42 +0100

    add vue-router

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import { createWebHistory, createRouter } from 'vue-router';
+
+import App from '@/App.vue';
+
+const routes = [{ path: '/', component: App }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
